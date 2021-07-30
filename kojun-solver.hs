96cfc72678599f441fd246ec799c063930a53e9a
Samuel Moreira Ransolin <samuransolin@gmail.com>
2021-07-29 22:27:48 -0300

    a
    Co-authored-by: Eduardo <ebsiqueira@users.noreply.github.com>
    Co-authored-by: leolperes <leolperes@users.noreply.github.com>

--- kojun-solver.hs
+++ kojun-solver.hs
@@ -2,11 +2,17 @@ module Main where
 
 import Reader
 import Matrix
-
+import Data.List
 -----------------------------------------------------------------------------
--- EXEMPLO DE RESOLUÇÃO PARA SUDOKU
+type Choices = [Value]
 
--- type Choices = [Value]
+-- ---------------------- solucao 2
+-- -- expanding choices one square at
+-- -- a time, and filtering out any resulting matrices that are blocked
+-- -- before considering any further choices.
+-- -- solve :: Grid -> [Grid]
+-- solve :: Grid -> Grid -> [Grid]
+-- solve vals pos = search (prune (choices vals pos) pos) pos
 
 -- -- primeira solução - INEFICIENTE
 -- -- cria uma matriz com todas as escolhas possiveis,
@@ -16,92 +22,93 @@ import Matrix
 -- solve :: Grid -> [Grid]
 -- solve = filter valid . collapse . fix prune . choices
 
--- valid :: Grid -> Bool
--- valid g = all noDuplicates (rows g) &&
---           all noDuplicates (cols g) &&
---           all noDuplicates (boxes g)
+solve :: Grid -> Grid -> [Grid]
+solve vals pos = filter (`safe` pos) (collapse (fix (`prune` pos) (choices vals pos)))
 
--- noDuplicates :: Eq a => [a] -> Bool
--- noDuplicates [] = True
--- noDuplicates (x:xs) = not (elem x xs) && noDuplicates xs
+-- itera a função (a -> a) no parametro de entrada até a saída ser igual a ele
+fix :: Eq a => (a -> a) -> a -> a
+fix f x = if x == x' then x else fix f x'
+    where x' = f x
 
--- -- preenche cada celula sem valor com uma lista de 1..9
--- choices :: Grid -> Matrix Choices
--- choices choices g = map (map choice) g
---     where choice v = if v == 0 then [1..9] else [v]
+-- preenche cada celula sem valor com uma lista de 1..9
+choices :: Grid -> Grid -> Matrix Choices
+choices vals pos = map (map choice) (zipWith zip vals pos)
+    where choice (v, p) = if v == 0 then [1..(lengthOfBlock p pos)] else [v]
 
--- -- retorna uma lista contendo todas as matrizes de solução possíveis
--- collapse :: Matrix [a] -> [Matrix a]
--- collapse m = cp (map cp m)
+-- de uma matriz, elimina das escolhas os valores que já foram utilizados no bloco, coluna e linha
+prune :: Matrix Choices -> Grid -> Matrix Choices
+prune vals pos = cols $ colsOfBlocksByCols (map reduce (blocksByCols vals pos)) (size vals)
 
--- -- retorna o produto cartesiano das listas de uma lista
--- -- ex: [[1, 2], [3, 4]] -> [[1, 3], [1, 4], [2, 3], [2, 4]]
--- cp :: [[a]] -> [[a]]
--- cp [] = [[]]
--- cp (xs:xss) = [y:ys | y <- xs, ys <- cp xss]
+-- de uma linha, reduz as escolhas com base em elementos unitários
+-- ex: ["1 2 3 4", "1", "3 4", "3"] -> ["2 4", "1", "4", "3"]
+reduce :: Row Choices -> Row Choices
+reduce xss = [xs `minus` singles | xs <- xss]
+    where singles = concat (filter single xss)
 
--- -- de uma matriz, elimina das escolhas os valores que já foram utilizados no bloco, coluna e linha
--- prune :: Matrix Choices -> Matrix Choices
--- prune = pruneBy boxes . pruneBy cols . pruneBy rows
---     where pruneBy f = f . map reduce . f
+minus :: Choices -> Choices -> Choices
+xs `minus` ys = if single xs then xs else xs \\ ys
 
--- -- de uma linha, reduz as escolhas com base em elementos unitários
--- -- ex: ["1 2 3 4", "1", "3 4", "3"] -> ["2 4", "1", "4", "3"]
--- reduce :: Row Choices -> Row Choices
--- reduce xss = [xs `minus` singles | xs <- xss]
---     where singles = concat (filter single xss)
+--retorna true se a lista passada só possui um elemento
+single :: [a] -> Bool
+single [_] = True
+single _ = False
 
--- minus :: Choices -> Choices -> Choices
--- xs `minus` ys = if single xs then xs else xs \\ ys
+search :: Matrix Choices -> Grid -> [Grid]
+search m pos
+    | blocked m pos = []
+    | all (all single) m = collapse m
+    | otherwise = [g | m' <- expand m, g <- search (prune m' pos) pos]
 
--- -- itera a função (a -> a) no parametro de entrada até a saída ser igual a ele
--- fix :: Eq a => (a -> a) -> a -> a
--- fix f x = if x == x' then x else fix f x'
---     where x' = f x
+-- matrizes bloqueadas nunca podem fornecer uma solução
+blocked :: Matrix Choices -> Grid -> Bool
+blocked m pos = void m || not (safe m pos)
 
--- ---------------------- solucao 2
--- -- expanding choices one square at
--- -- a time, and filtering out any resulting matrices that are blocked
--- -- before considering any further choices.
--- solve2 :: Grid -> [Grid]
--- solve = search . prune . choices
+-- verifica se ha alguma celula vazia na matriz
+void :: Matrix Choices -> Bool
+void m = any (any null) m
 
--- -- verifica se ha alguma celula vazia na matriz
--- void :: Matrix Choices -> Bool
--- void m = any (any null) m
+-- verifica se todos os campos sao consistentes (nao possuem valores unitarios duplicados)
+safe :: Matrix Choices -> Grid -> Bool
+safe m pos = all (check validNeighborhood) (cols m) &&
+        all (check validNeighborhood) (rows m) &&
+        all (check nodups) (blocks m pos) &&
+        all (check isDecreasing) (blocksByCols m pos)
+            where check f = f . concat . filter single
 
--- -- verifica se todos os campos sao consistentes (nao possuem valores unitarios duplicados)
--- safe :: Matrix Choices -> Bool
--- safe m = all consistent (rows m) &&
---          all consistent (cols m) &&
---          all consistent (boxes m)
+-- verifica se nao ha valores vizinhos iguais
+validNeighborhood :: Eq a => [a] -> Bool
+validNeighborhood [] = True
+validNeighborhood [a] = True
+validNeighborhood (a:b:bs) = if a == b then False else validNeighborhood bs
 
--- -- verifica se não há valores unitarios duplicados na linha passada
--- consistent :: Row Choices -> Bool
--- consistent = nodups . concat . filter single
+-- verifica se não há valores unitarios duplicados na linha passada
+nodups :: Eq a => [a] -> Bool
+nodups [] = True
+nodups (x:xs) = not (elem x xs) && nodups xs
 
--- -- matrizes bloqueadas nunca podem fornecer uma solução
--- blocked :: Matrix Choices -> Bool
--- blockes m = void m || not (safe m)
+-- verifica se nao ha valores vizinhos iguais
+isDecreasing :: Ord a => [a] -> Bool
+isDecreasing [] = True
+isDecreasing [a] = True
+isDecreasing (a:b:bs) = if a < b then False else isDecreasing bs
 
--- search :: Matrix Choices -> [Grid]
--- search m
---     | blocked m = []
---     | all (all single) m = collapse m
---     | otherwise = [g | m' <- expand m, g <- search (prune m')]
+-- retorna uma lista contendo todas as matrizes de solução possíveis
+collapse :: Matrix [a] -> [Matrix a]
+collapse m = cp (map cp m)
 
--- --retorna true se a lista passada só possui um elemento
--- single :: [a] -> Bool
--- single [_] = true
--- single _ = false
+-- retorna o produto cartesiano das listas de uma lista
+-- ex: [[1, 2], [3, 4]] -> [[1, 3], [1, 4], [2, 3], [2, 4]]
+cp :: [[a]] -> [[a]]
+cp [] = [[]]
+cp (xs:xss) = [y:ys | y <- xs, ys <- cp xss]
 
--- --The function expand behaves in the same way as collapse, except that
--- --it only collapses the first square with more than one choice:
--- expand :: Matrix Choices -> [Matrix Choices]
--- expand m = [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
---     where
---         (rows1,row:rows2) = break (any (not . single)) m
---         (row1,cs:row2)    = break (not . single) row
+--The function expand behaves in the same way as collapse, except that
+--it only collapses the first square with more than one choice:
+expand :: Matrix Choices -> [Matrix Choices]
+expand m = [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
+    where
+        (rows1,row:rows2) = break (any (not . single)) m
+        (row1,cs:row2)    = break (not . single) row
 
 ------------------------------------------------------------------------------
 
@@ -116,5 +123,10 @@ main = do
     (tabuleiro, posicoes) <- readPuzzle "tabuleiro.txt"
     -- print tabuleiro
     -- print posicoes
-    print $ blocks tabuleiro posicoes
-    print $ colsByBlocks tabuleiro posicoes
+    -- print $ blocks tabuleiro posicoes
+    -- print $ blocksByCols tabuleiro posicoes
+    -- print $ lengthOfBlock 1 posicoes
+    -- print $ choices tabuleiro posicoes
+    -- print $ cols $ colsOfBlocksByCols (blocksByCols tabuleiro posicoes) (size tabuleiro)
+    print $ solve tabuleiro posicoes
+    
--- modules/matrix.hs
+++ modules/matrix.hs
@@ -8,6 +8,9 @@ type Matrix a = [Row a]
 type Row a = [a]
 type Value = Int
 
+size :: Matrix a -> Int
+size m = length (m!!0)
+
 -- Retorna as linhas de uma matriz
 rows :: Matrix a -> [Row a]
 rows m = m
@@ -17,15 +20,23 @@ cols :: Matrix a -> [Row a]
 cols m = transpose m
 
 -- retorna os blocos em forma de linhas
-blocks :: Eq a => Matrix a -> Matrix a -> [Row a]
+blocks :: Eq a => Matrix a -> Grid -> [Row a]
 blocks vals pos = [filterByGroup group tupleValueGroup | group <- groups]
   where
-    tupleValueGroup = foldl1 (++) $ zipWith zip vals pos
-    groups = nub $ map snd tupleValueGroup
+    tupleValueGroup = foldl1 (++) (zipWith zip vals pos)
+    groups = nub (map snd tupleValueGroup)
     filterByGroup group list = map fst $ filter ((== group) . snd) list
 
+blocksByCols :: Eq a => Matrix a -> Grid -> [Row a]
+blocksByCols vals pos = zip (cols vals) (cols pos) >>= (\(v, p) -> blocks [v] [p])
 
-colsByBlocks :: Eq a => Matrix a -> Matrix a -> [Row a]
-colsByBlocks vals pos = zip (cols vals) (cols pos) >>= (\(v, p) -> blocks [v] [p])
--- equivalente a: concat [blocks ([v], [p]) | (v, p) <- zip (cols vals) (cols pos)]
--- >>= : [a] -> (a -> [b]) -> [b]
+lengthOfBlock :: Eq a => a -> Matrix a -> Int
+lengthOfBlock _ [] = 0
+lengthOfBlock id pos = sum [count id p | p <- pos]
+  where count x xs = length (filter (==x) xs)
+
+colsOfBlocksByCols :: [Row a] -> Int -> [Row a]
+colsOfBlocksByCols bs n = chunksOf n (concat bs)
+
+chunksOf :: Int -> [a] -> [Row a]
+chunksOf n = takeWhile (not . null) . map (take n) . iterate (drop n)
